9fb700867202f86d1a9deed26c7b710f98e6208b
Prakash Kumar <tecnocrates006@gmail.com>
2024-11-24 23:20:08 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+import Navbar from "@/component/Navbar";
+
+export default function Home() {
+  return (
+
+  <div>
+    <Navbar/>
+    <h1 className=" font-bold">Find Tutor</h1>
+    <Link href={"/register"}>Register pag</Link>
+  </div>
+  );
+}
