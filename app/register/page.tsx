c80522b1cbe0d0f5dd591c4c40553b173787be61
coder-crew <vikasjha@gmail.com>
2024-12-04 01:38:14 +0530

    register page to teacher

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,12 +1,34 @@
 import React from 'react'
 
+
 const page = () => {
   return (
-    <div>
-      <h1>Register page</h1>
-      <div>hello</div>
-    </div>
+    <form  style={{ maxWidth: "420px", margin: "50px auto", padding: "25px", border: "1px solid #ccc", borderRadius: "10px"}}>
+      <h3 style={{ marginBottom: "15px", display:"flex", justifyContent:"center"}}>Register for tutior <a href='#'>/Student</a></h3>
+      <div style={{ marginBottom: "15px" }}>
+        <label htmlFor= "userName" style={{ display: "flex", marginBottom: "5px"}}>Name :</label>
+        <input type='text' placeholder='Enter your name' id='userName'
+                    style={{ width: "100%", padding: "8px", borderRadius: "4px", border: "1px solid #ccc" ,color:"black"}}/>
+      </div>
+      <div style={{ marginBottom: "15px" }}>
+      <label htmlFor= "email" style={{ display: "flex", marginBottom: "5px"}}> email :</label>
+      <input type='email' placeholder='Enter your email' id='email'
+      style={{ width: "100%", padding: "8px", borderRadius: "4px", border: "1px solid #ccc" ,color:"black" }}/>
+      </div>
+      <div style={{ marginBottom: "15px" }}>
+      <label htmlFor= "pass" style={{ display: "flex", marginBottom: "5px"}}>Password :</label>
+      <input type='password' placeholder='Enter your password' id='pass'
+      style={{ width: "100%", padding: "8px", borderRadius: "4px", border: "1px solid #ccc" ,color:"black" }}/>
+      </div>
+      <div style={{ marginBottom: "15px" }}>
+      <label htmlFor= "con-pass" style={{ display: "flex", marginBottom: "5px"}}> Confirm password :</label>
+      <input type='password' placeholder='Enter your password' id='con-pass'
+      style={{ width: "100%", padding: "8px", borderRadius: "4px", border: "1px solid #ccc",color:"black" }}/>
+      </div>
+      <button>Submit</button>
+  
+    </form>
   )
 }
 
-export default page
+export default page;
