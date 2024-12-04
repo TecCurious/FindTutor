2139bca31d75a7a6aa5faee3d4bf8ed7967a6fcc
coder-crew <vikasjha@gmail.com>
2024-12-04 19:28:46 +0530

    student reg page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
 import Navbar from "@/component/Navbar";
+import page from "./studentReg/page";
+
 
 export default function Home() {
   return (
 
   <div>
     <Navbar/>
+    
     <h1 className=" font-bold">Find Tutor</h1>
     <Link href={"/register"}>Register pag</Link>
+   
   </div>
   );
 }
--- app/studentReg/page.tsx
+++ app/studentReg/page.tsx
@@ -1,14 +1,15 @@
+"use client"
 import React from 'react'
 import Link from 'next/link';
 
-const page = () => {
+const Page = () => {
   return (
-    <form  style={{ maxWidth: "420px", margin: "50px auto", padding: "25px", border: "1px solid #ccc", borderRadius: "10px", }}>
-      <h3 style={{ marginBottom: "15px", display:"flex", justifyContent:"center"}}>Register for tutior  <Link href={"./studentReg"}>/student register</Link></h3>
+    <form  style={{ maxWidth: "420px", margin: "50px auto", padding: "25px", border: "1px solid #ccc", borderRadius: "10px"}}>
+      <h3 style={{ marginBottom: "15px", display:"flex", justifyContent:"center"}}>Register for student <Link href={"./register"}>/tutior</Link></h3>
       <div style={{ marginBottom: "15px" }}>
         <label htmlFor= "userName" style={{ display: "flex", marginBottom: "5px"}}>Name :</label>
         <input type='text' placeholder='Enter your name' id='userName'
-                    style={{ width: "100%", padding: "8px", borderRadius: "4px", border: "1px solid black" ,color:"black"}}/>
+                    style={{ width: "100%", padding: "8px", borderRadius: "4px", border: "1px solid #ccc" ,color:"black"}}/>
       </div>
       <div style={{ marginBottom: "15px" }}>
       <label htmlFor= "email" style={{ display: "flex", marginBottom: "5px"}}> email :</label>
@@ -31,4 +32,4 @@ const page = () => {
   )
 }
 
-export default page;
+export default Page;
